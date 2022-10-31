4081fa42c7ec91afa267ddf8dc129bc55bbf6fb2
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-10-31 15:47:11 +0700

    Add project files.

---
+++ exercise2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise2
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] AF = new int[25];
+        private int cmp_count = 0; //number of data comparasion
+        private int mov_count = 0; //number of data movements
+        
+        }
+    }
+
