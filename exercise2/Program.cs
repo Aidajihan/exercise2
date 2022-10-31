958a1dafb3ad95018faebf05ce1714ef2eef6316
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-10-31 15:57:16 +0700

    Menambahkan Void Main

--- exercise2/Program.cs
+++ exercise2/Program.cs
@@ -9,9 +9,29 @@ namespace exercise2
     class Program
     {
         //array of integers to hold values
-        private int[] AF = new int[25];
+        private int[] Aida = new int[25];
         private int cmp_count = 0; //number of data comparasion
         private int mov_count = 0; //number of data movements
+
+        //number of elements in array
+        private int n;
+
+        void read()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of elements in array : ");
+                string s = Console.ReadLine();
+                n = int.Parse(s);
+                if (n <= 25)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements\n");
+
+
+            }
+
+        }
         }
     }
 
